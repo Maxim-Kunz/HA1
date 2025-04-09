3365cb007c622049d3c5a7ce0c9c7a964a160eeb
Rakiol <ramieter.a@gmail.com>
2025-04-09 18:15:15 +0200

    added 1 green test

--- app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
+++ app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
@@ -90,5 +90,21 @@ class CalculatorTest {
 
 
     //TODO hier weitere Tests erstellen
-}
+    @Test
+    @DisplayName("should multiply with the equals key")
+    void testMultiplyWithTheEqualsKey() {
+        Calculator calc = new Calculator();
 
+        calc.pressDigitKey(2);
+        calc.pressBinaryOperationKey("x");
+        calc.pressDigitKey(2);
+        calc.pressEqualsKey();
+        calc.pressEqualsKey();
+
+        String expected = "8";
+        String actual = calc.readScreen();
+
+        assertEquals(expected, actual);
+    }
+
+}
